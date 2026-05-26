c14c7e6655212e0455f20064399295cd92b1fd63
mantuanpietro <@mantuanpietro>
2026-05-26 09:49:38 -0300

    port

--- app.py
+++ app.py
@@ -138,8 +138,8 @@ def generate():
 
 # Rodar servidor
 if __name__ == "__main__":
-
     app.run(
-        debug=True,
-        port=5001
+        host="0.0.0.0",
+        port=5001,
+        debug=True
     )
